15b25d68c68d328176f958df17b7213ae4123145
AlanAguil <20233tn135@utez.edu.mx>
2024-10-20 23:18:11 -0600

    Mini integradora derick

---
+++ ListaDeTareas/src/main/java/com/intellij/listadetareas/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.intellij.listadetareas;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
